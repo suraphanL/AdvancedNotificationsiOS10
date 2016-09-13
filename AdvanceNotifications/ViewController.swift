637b29636827d742ab90da55c84294668b542e1d
Suraphan Laokondee <suraphan.l@kaidee.com>
2016-09-13 23:04:01 +0700

    Initial Commit

---
+++ AdvanceNotifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdvanceNotifications
+//
+//  Created by Suraphan Laokondee on 9/13/2559 BE.
+//  Copyright © 2559 Suraphan Laokondee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
